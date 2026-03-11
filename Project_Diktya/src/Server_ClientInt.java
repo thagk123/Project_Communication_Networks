057d3ae6a4e5d0f0e01b8075603d0e7726e8fd09
gkoutisa <115290334+thagk123@users.noreply.github.com>
2026-03-11 17:06:50 +0200

    Refactor Server_ClientInt interface methods and imports
    Updated method signatures and changed data structures for account management.

--- Project_Diktya/src/Server_ClientInt.java
+++ Project_Diktya/src/Server_ClientInt.java
@@ -1,21 +1,21 @@
+package com.messaging.server;
+
 import java.rmi.*;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public interface Server_ClientInt extends Remote {
 
-      public HashMap<Integer,Account> accs = new HashMap<>();
+      public Map<Integer,Account> getAccs() throws RemoteException;
 
-      public HashMap<Integer,Account> getAccs() throws RemoteException;
+      public int createAccount(String username) throws RemoteException;
 
-      public int CreateAccount(String username) throws RemoteException;
+      public String sendMessage(String message, String username, int authToken,Account account) throws RemoteException;
 
-      public String SendMessage(String message, String username, int authToken,Account account) throws RemoteException;
+      public List<Message> showInbox(int authToken) throws RemoteException;
 
-      public ArrayList<Message> ShowInbox(int authToken) throws RemoteException;
+      public String readMessage(int authToken, int messageId) throws RemoteException;
 
-      public String ReadMessage(int authToken, int message_id) throws RemoteException;
-
-      public String  DeleteMessage(int authToken, int message_id) throws RemoteException;
+      public String deleteMessage(int authToken, int messageId) throws RemoteException;
 }
